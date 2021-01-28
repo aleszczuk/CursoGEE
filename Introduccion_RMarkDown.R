5e97bd9a64b1470a0e115515961997e2e198fa02
Andres Leszczuk <aleszczuk@agro.uba.ar>
2021-01-28 15:36:15 -0300

    Actualizacion de encabezado YAML

--- Introduccion_RMarkDown.R
+++ Introduccion_RMarkDown.R
@@ -5,3 +5,15 @@ install.packages("rmarkdown")
 install.packages("tinytex")
 
 
+# R Markdown esta pensado para ciencas de datos "Data science"
+# por dos razones principales:
+# * Podes ejecutar codigo en R
+# * Obtener informacion y generar reportes automaticamente para tu audiencia
+
+
+# Y se debe crear en File --> new file --> R markdown
+
+# Posteriormente al hacer dicho Archivo se generan los 3 componentes de un archivo R markdown
+# Metadata, texto, y el codigo. 
+
+# Metadata se encuentra entre los --- (tres guiones medios)
